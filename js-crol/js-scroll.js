93bd30a657e741ac797439d3895fae08e808ea43
Farits <fshagiev@gmail.com>
2017-11-16 10:46:04 +0300

    js-scroll
    jq плагин для плавного перехода по ссылкам в меню шапки сайта.

---
+++ js-crol/js-scroll.js
@@ -0,0 +1,8 @@
+// плавный скрол к блоку
+$(function(){
+	$('a[href^="#"]').click(function(){
+		var target = $(this).attr('href');
+    $('html, body').animate({scrollTop: $(target).offset().top}, 800);//800 - длительность скроллинга в мс
+    return false; 
+}); 
+});
